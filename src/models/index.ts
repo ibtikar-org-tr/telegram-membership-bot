eca96b5383df5bc5d66170e73499a687cf23fa86
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-10-14 03:02:07 +0300

    refactor: remove User model and CRUD operations for task-follower

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,3 @@
-export { User, UserModel } from './task-follower/user';
 export { Activity, ActivityModel } from './task-follower/activity';
 export { Sheet, SheetModel } from './task-follower/sheet';
 export { Task, TaskModel } from './task-follower/task';
